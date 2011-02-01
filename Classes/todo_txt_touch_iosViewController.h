9e316a20ad810c6513c41253feeb04893dbe5c1d
Shawn McGuire <bigbash@Shawn-McGuires-iMac.local>
2011-02-01 09:04:33 +0800

    Blank Xcode View Based app with upgrade to Universal

---
+++ Classes/todo_txt_touch_iosViewController.h
@@ -0,0 +1,16 @@
+//
+//  todo_txt_touch_iosViewController.h
+//  todo.txt-touch-ios
+//
+//  Created by Shawn McGuire on 1/31/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface todo_txt_touch_iosViewController : UIViewController {
+
+}
+
+@end
+
