52ecfd3e18d666ef151894c2a369af374d1e90ec
Charles Jones <cbj3@chuckbjones.com>
2011-08-04 03:01:47 -0500

    Added missing files for Task and Priority classes

---
+++ Classes/Priority.m
@@ -0,0 +1,128 @@
+/**
+ *
+ * Todo.txt-Touch-iOS/Classes/todo_txt_touch_iosAppDelegate.h
+ *
+ * Copyright (c) 2009-2011 Gina Trapani, Shawn McGuire
+ *
+ * LICENSE:
+ *
+ * This file is part of Todo.txt Touch, an iOS app for managing your todo.txt file (http://todotxt.com).
+ *
+ * Todo.txt Touch is free software: you can redistribute it and/or modify it under the terms of the GNU General Public
+ * License as published by the Free Software Foundation, either version 2 of the License, or (at your option) any
+ * later version.
+ *
+ * Todo.txt Touch is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied
+ * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along with Todo.txt Touch.  If not, see
+ * <http://www.gnu.org/licenses/>.
+ *
+ * @author Gina Trapani <ginatrapani[at]gmail[dot]com>
+ * @author Shawn McGuire <mcguiresm[at]gmail[dot]com> 
+ * @license http://www.gnu.org/licenses/gpl.html
+ * @copyright 2009-2011 Gina Trapani, Shawn McGuire
+ *
+ *
+ * Copyright (c) 2011 Gina Trapani and contributors, http://todotxt.com
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files (the
+ * "Software"), to deal in the Software without restriction, including
+ * without limitation the rights to use, copy, modify, merge, publish,
+ * distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject to
+ * the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+ * LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+ * OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+ * WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+#import "Priority.h"
+
+static NSArray* priorityArray = nil;
+
+@implementation Priority
+
+@synthesize code, listFormat, detailFormat, fileFormat;
+
+- (id)initWithCode:(NSString*)theCode:(NSString*)theListFormat:(NSString*)theDetailFormat:(NSString*)theFileFormat {
+	self = [super init];
+	if (self) {
+        code = [theCode retain];
+        listFormat = [theListFormat retain];
+        detailFormat = [theDetailFormat retain];
+        fileFormat = [theFileFormat retain];	
+	}
+	return self;
+}
+
+- (NSUInteger)hash {
+	return [code hash];
+}
+
++ (void)initialize {
+	@synchronized(self) {
+		if (!priorityArray) {
+			priorityArray = [NSArray arrayWithObjects:
+							 [[Priority alloc] initWithCode:@"-" :@" " :@"" :@""],
+							 [[Priority alloc] initWithCode:@"A" :@"A" :@"A" :@"(A)"],
+							 [[Priority alloc] initWithCode:@"B" :@"B" :@"B" :@"(B)"],
+							 [[Priority alloc] initWithCode:@"C" :@"C" :@"C" :@"(C)"],
+							 [[Priority alloc] initWithCode:@"D" :@"D" :@"D" :@"(D)"],
+							 [[Priority alloc] initWithCode:@"E" :@"E" :@"E" :@"(E)"],
+							 [[Priority alloc] initWithCode:@"F" :@"F" :@"F" :@"(F)"],
+							 [[Priority alloc] initWithCode:@"G" :@"G" :@"G" :@"(G)"],
+							 [[Priority alloc] initWithCode:@"H" :@"H" :@"H" :@"(H)"],
+							 [[Priority alloc] initWithCode:@"I" :@"I" :@"I" :@"(I)"],
+							 [[Priority alloc] initWithCode:@"J" :@"J" :@"J" :@"(J)"],
+							 [[Priority alloc] initWithCode:@"K" :@"K" :@"K" :@"(K)"],
+							 [[Priority alloc] initWithCode:@"L" :@"L" :@"L" :@"(L)"],
+							 [[Priority alloc] initWithCode:@"M" :@"M" :@"M" :@"(M)"],
+							 [[Priority alloc] initWithCode:@"N" :@"N" :@"N" :@"(N)"],
+							 [[Priority alloc] initWithCode:@"O" :@"O" :@"O" :@"(O)"],
+							 [[Priority alloc] initWithCode:@"P" :@"P" :@"P" :@"(P)"],
+							 [[Priority alloc] initWithCode:@"Q" :@"Q" :@"Q" :@"(Q)"],
+							 [[Priority alloc] initWithCode:@"R" :@"R" :@"R" :@"(R)"],
+							 [[Priority alloc] initWithCode:@"S" :@"S" :@"S" :@"(S)"],
+							 [[Priority alloc] initWithCode:@"T" :@"T" :@"T" :@"(T)"],
+							 [[Priority alloc] initWithCode:@"U" :@"U" :@"U" :@"(U)"],
+							 [[Priority alloc] initWithCode:@"V" :@"V" :@"V" :@"(V)"],
+							 [[Priority alloc] initWithCode:@"W" :@"W" :@"W" :@"(W)"],
+							 [[Priority alloc] initWithCode:@"X" :@"X" :@"X" :@"(X)"],
+							 [[Priority alloc] initWithCode:@"Y" :@"Y" :@"Y" :@"(Y)"],
+							 [[Priority alloc] initWithCode:@"Z" :@"Z" :@"Z" :@"(Z)"],
+							 nil
+							 ];
+		}
+	}
+}
+
++ (Priority*)priorityWithName:(PriorityName)name {
+	return [priorityArray objectAtIndex:name];
+}
+
++ (Priority*)NONE {
+	return [Priority priorityWithName:PriorityNone];
+}
+
++ (Priority*)priorityWithCode:(NSString*)code {
+	for (int i = 0; i < [priorityArray count]; i++) {
+		Priority *priority = [priorityArray objectAtIndex:i];
+		if ([[priority code] caseInsensitiveCompare:code] == NSOrderedSame) {
+			return priority;
+		}
+	}
+	return [Priority NONE];
+}
+
+@end
---
+++ Classes/Task.h
@@ -0,0 +1,96 @@
+/**
+ *
+ * Todo.txt-Touch-iOS/Classes/todo_txt_touch_iosAppDelegate.h
+ *
+ * Copyright (c) 2009-2011 Gina Trapani, Shawn McGuire
+ *
+ * LICENSE:
+ *
+ * This file is part of Todo.txt Touch, an iOS app for managing your todo.txt file (http://todotxt.com).
+ *
+ * Todo.txt Touch is free software: you can redistribute it and/or modify it under the terms of the GNU General Public
+ * License as published by the Free Software Foundation, either version 2 of the License, or (at your option) any
+ * later version.
+ *
+ * Todo.txt Touch is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied
+ * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along with Todo.txt Touch.  If not, see
+ * <http://www.gnu.org/licenses/>.
+ *
+ * @author Gina Trapani <ginatrapani[at]gmail[dot]com>
+ * @author Shawn McGuire <mcguiresm[at]gmail[dot]com> 
+ * @license http://www.gnu.org/licenses/gpl.html
+ * @copyright 2009-2011 Gina Trapani, Shawn McGuire
+ *
+ *
+ * Copyright (c) 2011 Gina Trapani and contributors, http://todotxt.com
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files (the
+ * "Software"), to deal in the Software without restriction, including
+ * without limitation the rights to use, copy, modify, merge, publish,
+ * distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject to
+ * the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+ * LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+ * OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+ * WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+#import <Foundation/Foundation.h>
+#import "Priority.h"
+
+
+@interface Task : NSObject {
+	NSString *originalText;
+	Priority *originalPriority;
+	
+	NSUInteger taskId;
+	Priority *priority;
+	BOOL deleted;
+	BOOL completed;
+	NSString *text;
+	NSString *completionDate;
+	NSString *prependedDate;
+	NSString *relativeAge;
+	NSArray *contexts;
+	NSArray *projects;	
+}
+
+@property (nonatomic, readonly) NSString *originalText;
+@property (nonatomic, readonly) Priority *originalPriority;
+@property (nonatomic, readonly) NSUInteger taskId;
+@property (nonatomic, readonly) BOOL deleted;
+@property (nonatomic, readonly) BOOL completed;
+@property (nonatomic, readonly) NSString *text;
+@property (nonatomic, readonly) NSString *completionDate;
+@property (nonatomic, readonly) NSString *prependedDate;
+@property (nonatomic, readonly) NSString *relativeAge;
+@property (nonatomic, readonly) NSArray *contexts;
+@property (nonatomic, readonly) NSArray *projects;
+
+@property (nonatomic, retain) Priority *priority;
+
+- (id)initWithId:(NSUInteger)newID withRawText:(NSString*)rawText withDefaultPrependedDate:(NSDate*)date;
+- (id)initWithId:(NSUInteger)taskID withRawText:(NSString*)rawText;
+- (void)update:(NSString*)rawText;
+- (void)markComplete:(NSDate*)date;
+- (void)markIncomplete;
+- (void)deleteTask;
+- (NSString*)inScreenFormat;
+- (NSString*)inFileFormat;
+- (void)copyInto:(Task*)destination;
+- (BOOL)isEqual:(id)anObject;
+- (NSUInteger)hash;
+
+@end
