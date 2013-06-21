a5b7ece24ba32770887ee502b6443b191175c6e2
Brendon Justin <bjustin@nerdery.com>
2013-06-21 10:03:48 -0500

    Delete commented out method overrides.

--- View Controllers/TasksViewController.m
+++ View Controllers/TasksViewController.m
@@ -151,25 +151,6 @@ static NSString *const kCellIdentifier = @"FlexiTaskCell";
 	}
 }
 
-/*
-// The designated initializer. Override to perform setup that is required before the view is loaded.
-- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil {
-    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
-    if (self) {
-        // Custom initialization
-    }
-    return self;
-}
-*/
-
-/*
-// Implement loadView to create a view hierarchy programmatically, without using a nib.
-- (void)loadView {
-}
-*/
-
-
-
 // Implement viewDidLoad to do additional setup after loading the view, typically from a nib.
 - (void)viewDidLoad {
     [super viewDidLoad];
