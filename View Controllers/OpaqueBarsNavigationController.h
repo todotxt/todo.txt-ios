b770cdc786a0490be0ddfc3f99bb8976dfed9b7f
Brendon Justin <brendonjustin@gmail.com>
2013-10-05 13:49:06 -0500

    Use a navigation controller subclass to turn off translucency for navigation bars and toolbars.

---
+++ View Controllers/OpaqueBarsNavigationController.h
@@ -0,0 +1,13 @@
+//
+//  OpaqueBarsNavigationController.h
+//  todo.txt-touch-ios
+//
+//  Created by Brendon Justin on 10/5/13.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface OpaqueBarsNavigationController : UINavigationController
+
+@end
